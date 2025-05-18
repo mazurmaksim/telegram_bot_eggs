fbb502edd4d2c5f8f561e3abe85b722c36d290a4
maksym <maksimka37@gmail.com>
2025-05-19 00:27:23 +0300

    Bot initializer

--- src/main/java/ua/maks/prog/bot/BotInitializer.java
+++ src/main/java/ua/maks/prog/bot/BotInitializer.java
@@ -1,18 +1,26 @@
 package ua.maks.prog.bot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Component
-public class BotInitializer {
+public class BotInitializer implements CommandLineRunner {
+
+    private final ChatBot bot;
 
     public BotInitializer(ChatBot bot) {
+        this.bot = bot;
+    }
+
+    @Override
+    public void run(String... args) {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(bot);
-            System.out.println("Bot successfully started!");
+            System.out.println("✅ Telegram bot successfully started!");
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
